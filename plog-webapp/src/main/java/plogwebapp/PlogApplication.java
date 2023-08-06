3bbaa9dd23be6514034e8d884e900728fc7843ba
sungminyang <lonesome661@naver.com>
2023-08-06 16:52:11 +0900

    spring boot webapp init

---
+++ plog-webapp/src/main/java/plogwebapp/PlogApplication.java
@@ -0,0 +1,13 @@
+package plogwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlogApplication.class, args);
+	}
+
+}
